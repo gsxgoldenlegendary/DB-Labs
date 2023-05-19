d84d0406e5892b43804d32b94a61c13b1390212a
Songxiao Guo <logname@mail.ustc.edu.cn>
2023-05-19 20:49:02 +0800

    test jbdc connection.

---
+++ Lab3/Frame.java
@@ -0,0 +1,12 @@
+public class Frame extends javax.swing.JFrame{
+    public Frame(){
+        super("Teaching & Researching Registration System");
+        this.setSize(960, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+    }
+}
